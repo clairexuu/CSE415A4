0da54720c74deacc66686a108974db20ffcf240a
Claire <xinyix26@uw.edu>
2025-02-16 15:42:23 -0800

    static_eval updated

--- xinyix26_KInARow.py
+++ xinyix26_KInARow.py
@@ -31,7 +31,7 @@ class OurAgent(KAgent):  # Keep the class name "OurAgent" so a game master
         if twin: self.long_name += ' II'
         self.persona = 'agressive'
         self.voice_info = {'Chrome': 10, 'Firefox': 2, 'other': 0}
-        self.playing = "don't know yet" # e.g., "X" or "O".
+        self.playing = "X" # e.g., "X" or "O".
         self.alpha_beta_cutoffs_this_turn = -1
         self.num_static_evals_this_turn = -1
         self.zobrist_table_num_entries_this_turn = -1
@@ -110,14 +110,15 @@ class OurAgent(KAgent):  # Keep the class name "OurAgent" so a game master
         # etc. 
 
     def static_eval(self, state, game_type=None):
-        # Values should be higher when the states are better for X,
-        # lower when better for O.
+        # static evaluation function.
+        # Given a game state, it returns a numeric evaluation of that state
+        # where a higher value means that the state is better for X and a lower value is a state better for O.
         k = game_type.k  # k in a row/column/diagonal to win
         board = state.board
         rows, cols = len(board), len(board[0])
         game_name = game_type.short_name  # amount of work depend on type of game
 
-        # Precompute valid positions
+        # Get valid positions in the board
         if game_name == "5-in-a-Row":
             valid_positions = {(r, c) for r in range(rows) for c in range(cols) if not is_forbidden_corner(r, c)}
         elif game_name == "Cassini":
@@ -125,31 +126,45 @@ class OurAgent(KAgent):  # Keep the class name "OurAgent" so a game master
         else:
             valid_positions = {(r, c) for r in range(rows) for c in range(cols)}
 
-        def count_lines(player, k_needed):
-            # Count the number of open sequences of length k_needed for a given player
+        # TODO: to make it faster, mark row/col/dia that's already checked?
+
+        def count_lines(player, num_pieces):
+            # Count the number of open enough hor/ver/dia_up/dia_down sequence with exactly num_pieces of player
             count = 0
             for r, c in valid_positions:
                 if board[r][c] == player:  # Only check from positions occupied by the player
-                    count += check_directions(r, c, player, k_needed)
+                    count += check_directions(r, c, player, num_pieces)
             return count
 
-        def check_directions(r, c, player, k_needed):
-            # Checks sequences in four directions: horizontal, vertical,
-            #                                      lower-left to upper-right,  and upper-left to lower-right
-            directions = [(1, 0), (0, 1), (1, 1), (1, -1)]  # Down, right, diagonal-right, diagonal-left
+        def check_directions(r, c, player, num_pieces):
+            # Checks open enough sequences in four directions
+            # open enough means there's enough empty space (no player, no other player, and valid)
+            #                   to form a sequence of length k
+            directions = {'hor': get_board_row(r, board),
+                          'ver': get_board_col(c, board),
+                          'dia_up': get_board_dia_up(r, c, board),
+                          'dia_down' : get_board_dia_down(r, c, board)}
             count = 0
-            for dr, dc in directions:
-                sequence = []
-                for i in range(k):
-                    nr, nc = r + i * dr, c + i * dc
-                    if (nr, nc) in valid_positions and 0 <= nr < rows and 0 <= nc < cols:
-                        sequence.append(board[nr][nc])
-                    else:
-                        break
-                if (len(sequence) == k and
-                    sequence.count(player) == k_needed and
-                    ('X' if player == 'O' else 'O') not in sequence):
-                    count += 1
+
+            for dir_name, dir_elements in directions:
+                # for the diagonals, skip diagonals shorter than k
+                if len(dir_elements) < k:
+                    break
+
+                # Step 1: check if the current direction has exact num_pieces of player
+                if dir_elements.count(player) == num_pieces:
+                    # Step 2: check if there's enough empty space
+                    consecutive_sequence = 0 # need at least k
+                    for item in dir_elements:
+                        if item == "" or item == player:
+                            consecutive_sequence += 1
+                        else: # we meet other player or an obstacle
+                            consecutive_sequence = 0
+
+                        if consecutive_sequence >= k:
+                            count += 1
+                            break
+
             return count
 
         def compute_eval(player, k):
@@ -157,11 +172,13 @@ class OurAgent(KAgent):  # Keep the class name "OurAgent" so a game master
             #   count_lines of i items in an open enough row/column/diagonal * 10 ** (i-1) for 1 <= i <= k
             sum = 0
             for i in range(1, k+1):
-                sum += count_lines(player, i) * 10 ** (i-1)
+                sum += count_lines(player, i) * (10 ** (i-1))
             return sum
 
         # return eval of X - eval of O so that value is larger when state better for X, smaller when state better for O
         return compute_eval('X', k) - compute_eval('O', k)
+
+# Helper Functions
 
 def is_forbidden_corner(r, c):
     # check if a position is in the forbidden corner in FIAR
@@ -170,12 +187,59 @@ def is_forbidden_corner(r, c):
 def is_saturn_region(r, c):
     # check if a position is in saturn in Cassini
     return (r, c) in {(2, 3), (2, 4), (4, 3), (4, 4), (3, 2), (3, 3), (3, 4), (3, 5)}
- 
+
+# Figure out who the other player is.
+# For example, other("X") = "O"
+def other(p):
+    if p == 'X': return 'O'
+    return 'X'
+
+def get_board_col(j, board):
+    # return the column j of the board
+    if j < 0 or j > len(board[0]):
+        raise Exception("No such Column in this game board")
+    column = []
+    for i in range(len(board)):
+        column.append(board[i][j])
+    return column
+
+def get_board_row(i, board):
+    # return the row i of the board
+    if i < 0 or i > len(board):
+        raise Exception("No such Row in this game board")
+    return board[i]
+
+def get_board_dia_up(i, j, board):
+    # return the lower-left to upper-right diagonal of the board containing (i, j)
+    n = len(board)  # Number of rows
+    m = len(board[0])  # Number of columns
+    diag_sum = i + j # constant along the lower-left to upper-right diagonal
+    diagonal = []
+
+    for r in range(n):  # for every row
+        c = diag_sum - r # compute column index
+        if 0 <= c < m:  # if in the board
+            diagonal.append(board[r][c])
+    return diagonal
+
+
+def get_board_dia_down(i, j, board):
+    # return the upper-left to lower-right diagonal of the board containing (i, j)
+    n = len(board)  # Number of rows
+    m = len(board[0])  # Number of columns
+    diag_diff = i - j  # constant along the upper-left to lower-right diagonal
+    diagonal = []
+
+    for r in range(n):  # for every row
+        c = r - diag_diff # compute column index
+        if 0 <= c < m:  # if in the board
+            diagonal.append(board[r][c])
+    return diagonal
+
 # OPTIONAL THINGS TO KEEP TRACK OF:
 
-#  WHO_MY_OPPONENT_PLAYS = other(WHO_I_PLAY)
+ # WHO_MY_OPPONENT_PLAYS = other(WHO_I_PLAY)
 #  MY_PAST_UTTERANCES = []
 #  OPPONENT_PAST_UTTERANCES = []
 #  UTTERANCE_COUNT = 0
 #  REPEAT_COUNT = 0 or a table of these if you are reusing different utterances
-
